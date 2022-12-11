b2582e7bcecf6e6ccc5f369a51890ca43372bf54
hongxiaobin <2812181610@qq.com>
2022-12-11 20:25:53 +0800

    fix(OSSClient銆丼nowFlake): 修改OSS中获取不到配置文件参数的问题
    1.修复在OSSClient中引用不到配置文件参数的问题

--- src/test/java/com/douyin/OssTest.java
+++ src/test/java/com/douyin/OssTest.java
@@ -3,6 +3,13 @@ package com.douyin;
 import com.douyin.util.OssClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @Author hongxiaobin
@@ -14,5 +21,15 @@ public class OssTest {
     @Test
     public void ossTest() {
         System.out.println(OssClient.ossClientInitialization());
+    }
+
+    @Test
+    public void ossUploadTest() throws Exception {
+        Path path = Paths.get("D:\\桌面\\WeChat_20221211201424.mp4");
+        File file = new File(path.toUri());
+        FileInputStream fileInputStream = new FileInputStream(file);
+        MultipartFile multipartFile = new MockMultipartFile(file.getName(), fileInputStream);
+        String s = OssClient.uploadSource(multipartFile, "1.mp4");
+        System.out.println("访问路径为  " + s);
     }
 }
